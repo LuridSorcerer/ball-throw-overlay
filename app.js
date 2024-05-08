58ee0ff0b3fb628f003a78d8b3a02f47aecc9d57
Andrew Lingenfelter <luridsorcerer@gmail.com>
2024-05-08 12:58:25 -0400

    Added great and ultra balls

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ let ctx;
 let time;
 
 let balls = [];
+let balltypes = [];
 let pokeball; // pokeball image canvas
+let greatball;
+let ultraball;
 
 function init() {
     // get canvas and set the dimensions
@@ -49,6 +52,78 @@ function init() {
     pctx.beginPath();
     pctx.arc(32,32, 6, 0, Math.PI*2);
     pctx.fill();
+    
+    // create great ball image
+    greatball = document.createElement("canvas");
+    greatball.width = 64;
+    greatball.height = 64;
+    pctx = greatball.getContext("2d");
+    // draw the outline
+    pctx.fillStyle = "black";
+    pctx.beginPath();
+    pctx.arc(32,32,32,0,2*Math.PI);
+    pctx.fill();
+    // blue top half
+    pctx.fillStyle = "blue";
+    pctx.beginPath();
+    pctx.arc(32,30, 28, Math.PI,0);
+    pctx.fill();
+    // red stripes
+    pctx.fillStyle = "red";
+	pctx.fillRect(16,8,8,16);
+	pctx.fillRect(40,8,8,16);
+    // white bottom half
+    pctx.fillStyle = "white";
+    pctx.beginPath();
+    pctx.arc(32,34, 28, 0, Math.PI);
+    pctx.fill();
+    // black outline of button
+    pctx.fillStyle = "black";
+    pctx.beginPath();
+    pctx.arc(32,32, 8, 0, Math.PI*2);
+    pctx.fill();
+    // white button
+    pctx.fillStyle = "white";
+    pctx.beginPath();
+    pctx.arc(32,32, 6, 0, Math.PI*2);
+    pctx.fill();
+
+    // create ultra ball image
+    ultraball = document.createElement("canvas");
+    ultraball.width = 64;
+    ultraball.height = 64;
+    pctx = ultraball.getContext("2d");
+    // draw the outline
+    pctx.fillStyle = "black";
+    pctx.beginPath();
+    pctx.arc(32,32,32,0,2*Math.PI);
+    pctx.fill();
+    // black top half
+    pctx.fillStyle = "black";
+    pctx.beginPath();
+    pctx.arc(32,30, 28, Math.PI,0);
+    pctx.fill();
+    // yellow stripes
+    pctx.fillStyle = "yellow";
+	pctx.fillRect(16,8,8,16);
+	pctx.fillRect(40,8,8,16);
+    // white bottom half
+    pctx.fillStyle = "white";
+    pctx.beginPath();
+    pctx.arc(32,34, 28, 0, Math.PI);
+    pctx.fill();
+    // black outline of button
+    pctx.fillStyle = "black";
+    pctx.beginPath();
+    pctx.arc(32,32, 8, 0, Math.PI*2);
+    pctx.fill();
+    // white button
+    pctx.fillStyle = "white";
+    pctx.beginPath();
+    pctx.arc(32,32, 6, 0, Math.PI*2);
+    pctx.fill();
+    
+    balltypes = [pokeball,greatball,ultraball];
 }
 
 function update() {
@@ -79,7 +154,7 @@ function render () {
 		ctx.save();
 		ctx.translate(ball.location.x,ball.location.y);
 		ctx.rotate(ball.location.r*Math.PI/180);
-		ctx.drawImage(pokeball,-pokeball.width/2,-pokeball.height/2);
+		ctx.drawImage(balltypes[ball.type],-32,-32);
 		ctx.restore();
     });
 }
@@ -102,7 +177,8 @@ function add_ball() {
 			y:-1000*(0.5+Math.random()),
 			r:720*Math.random()
 		},
-		age: 0
+		age: 0,
+		type: Math.floor(Math.random() * 3)
     });
 }
 
